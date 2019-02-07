a81687375a047af2c45f936f5761cfde87bda85c
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-02-07 12:20:41 -0500

    Get delete button wired up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
+import STORE from './STORE'
 import List from './List'
 import './App.css';
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
-  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = STORE;
+  }
+
+  deleteCard = (cardID) => {
+    console.log(`Delete card ${cardID} from all lists`);
+  }
+
+  addRandomCard = () => {
+    console.log(`Add random card to list XXX`);
+  }
 
   render() {
-    const { store } = this.props
     return (
       <main className='App'>
         <header className='App-header'>
           <h1>Trelloyes!</h1>
         </header>
         <div className='App-list'>
-          {store.lists.map(list => (
+          {this.state.lists.map(list => (
             <List
               key={list.id}
               header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
+              cards={list.cardIds.map(id => this.state.allCards[id])}
+              onAddRandomClick={this.addRandomCard}
+              onDeleteCardClick={this.deleteCard}
             />
           ))}
         </div>
